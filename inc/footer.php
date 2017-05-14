2da0a793b626b1d1469a33052af9bbe4863ea750
phixyn <phixyn@gmail.com>
2017-05-14 15:21:40 +0100

    update project to use bootstrap

--- inc/footer.php
+++ inc/footer.php
@@ -1,29 +1,15 @@
-	<div class="footer" id="footer-800px">
-		<div class="copyright-msg">
-			&copy;
-			<?php 
-				$fromYear = 2014;
-				$thisYear = (int)date('Y'); 
-				echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : '');
-			?>
-			Alpeche Pancha. All Rights Reserved.
-		</div>
-		<div class="parent-clear"></div>	<!-- make parent element automatically expand to child elements' heights -->
-	</div>	<!-- .footer#footer-800px -->
+        <!-- Footer -->
+        <div class="jumbotron" id="footer">
+            <div class="container-fluid">
+                <div class="copyright-msg">
+                    &copy; <?php $fromYear = 2014; $thisYear = (int)date('Y'); echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : '');?> Phixyn & Zoey. All rights reserved
+                </div>
+            </div>
+        </div>
 
-	<div class="footer" id="footer-320px">
-		<p> <a href="#">Full site</a> &nbsp;//&nbsp; <a href="#">Work</a> &nbsp;//&nbsp; <a href="#">About</a> &nbsp;//&nbsp; <a href="#">Contact</a> </p>
-		<div class="copyright-msg">
-			&copy;
-			<?php 
-				$fromYear = 2014;
-				$thisYear = (int)date('Y'); 
-				echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : '');
-			?>
-			Alpeche Pancha. All Rights Reserved.
-		</div>
-		<div class="parent-clear"></div>	<!-- make parent element automatically expand to child elements' heights -->
-	</div>	<!-- .footer#footer-320px -->
-
-</body>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="<?php echo $js_url; ?>bootstrap.min.js"></script>
+    </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -1,61 +1,63 @@
-<?php
-$stylesheet_url="/redhawk/css/" . "style.css";
-$images_url="/redhawk/img/";
-$pages_url="/redhawk/pages/";
-$root_url="/redhawk/"; ?>
-
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="<?php echo $site_description; ?>" />
+        <meta name="keywords" content="<?php echo $site_keywords; ?>" />
+        <title><?php echo $page_title; ?></title>
 
-<head>
-	<!--[if lt IE 9]>
-	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<title><?php echo $page_title; ?></title>
-	<meta name="description" content="<?php echo $site_description; ?>" />
-	<meta name="keywords" content="<?php echo $site_keywords; ?>" />
-	<meta charset="iso-8859-1" />
-	<link rel="stylesheet" type="text/css" href="<?php echo $stylesheet_url; ?>" />
-</head>
+        <link href="<?php echo $css_url; ?>bootstrap.min.css" rel="stylesheet" />
+        <link href="<?php echo $css_url; ?>style.css" rel="stylesheet" />
 
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
-<body>
-	
-	<div class="header-wrapper" id="header-wrapper-800px">
-		<div class="row">
-			<div class="col" id="site-logo">PHIXYN</div> <!-- #site-logo -->
-			<nav class="col top-menu" id="top-menu-800px">
-				<ul>
-					<li class="col"><a href="#" title="Donate"> Donate </a></li>
-					<li class="col"><a href="<?php echo $pages_url; ?>contact.php" title="Contact">Contact</a></li>
-					<li class="col"><a href="<?php echo $pages_url; ?>about.php" title="About">About</a></li>
-					<li class="col"><a href="<?php echo $pages_url; ?>projects.php" title="Projects">Projects</a></li>
-					<li class="col"><a href="<?php echo $pages_url; ?>blog.php" title="Blog"> Blog </a></li>
-					<li class="col"><a class="current" href="<?php echo $root_url; ?>index.php" title="Home">Home</a></li>
-				</ul>
-			</nav>	<!-- #top-menu-800px -->
-		</div>		<!-- .row -->
-	</div>			<!-- #header-wrapper-800px -->
-	
-	<header>
-		<div class="col" id="social-icons">
-			<ul>
-				<li class="col"><a href="http://www.facebook.com/phixyn" title="Facebook"><img src="<?php echo $images_url; ?>social/facebook_dark.png" /></a></li>
-				<li class="col"><a href="http://www.twitter.com/Phixyn" title="Twitter"><img src="<?php echo $images_url; ?>social/twitter02_dark.png" /></a></li>
-				<li class="col"><a href="http://uk.linkedin.com/in/alpechepancha" title="LinkedIn"><img src="<?php echo $images_url; ?>social/in_dark.png" /></a></li>
-			</ul>
-		</div>	<!-- #social-icons -->
-	</header>
-	
-	<div class="header-wrapper" id="header-wrapper-320px">
-		<nav class="top-menu" id="top-menu-320px">
-			<ul>
-				<li><a href="<?php echo $root_url; ?>index.php" title="Home"><img src="<?php echo $images_url; ?>home-icon.png" /></a></li>
-				<li><a href="<?php echo $pages_url; ?>projects.php" title="Projects"><img src="<?php echo $images_url; ?>home-icon.png" /></a></li>
-				<li><a class="current" href="#" title="Chat"><img src="<?php echo $images_url; ?>home-icon.png" /></a></li>
-				<li><a href="<?php echo $pages_url; ?>about.php" title="About"><img src="<?php echo $images_url; ?>home-icon.png" /></a></li>
-				<li><a href="<?php echo $pages_url; ?>contact.php" title="Contact"><img src="<?php echo $images_url; ?>home-icon.png" /></a></li>
-			</ul>
-		</nav>	<!-- #top-menu-320px -->
-	</div>	<!-- #header-wrapper-320px -->
-	
+    <body>
+        <!-- Top navbar -->
+        <nav class="navbar navbar-inverse" id="top-navbar">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">PHIXYN</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="top-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li><a href="<?php echo $blog_url; ?>#">Blog</a></li>
+                        <li><a href="<?php echo $root_url; ?>#">Projects</a></li>
+                        <li><a href="https://www.twitch.tv/phixyn">Twitch</a></li>
+                        <li><a href="<?php echo $root_url; ?>#">About</a></li>
+                        <li><a href="<?php echo $root_url; ?>#">Contact</a></li>
+                        <li><a href="<?php echo $root_url; ?>#">Donate</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Social Media <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="https://twitter.com/Phixyn">Twitter</a></li>
+                                <li><a href="https://www.facebook.com/phixyn">Facebook</a></li>
+                                <li><a href="https://www.linkedin.com/in/alpechepancha">LinkedIn</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="https://github.com/Phixyn">Github</a></li>
+                                <li><a href="https://www.livecoding.tv/phixyn/">LiveCoding</a></li>
+                                <li><a href="https://www.youtube.com/user/Alpeche21/">Youtube</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div> <!-- /.navbar-collapse -->
+            </div> <!-- /.container-fluid -->
+        </nav> <!-- /#top-navbar -->
