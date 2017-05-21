b79d32fedf6d3173c779ee3ddd4e6ef74feeb34c
phixyn <phixyn@gmail.com>
2017-05-21 23:40:28 +0100

    WIP: update config

--- config/config.php
+++ config/config.php
@@ -1,23 +1,28 @@
 <?php
 
+$domain = "//phixyn.com";
 $config = array(
     "page_title" => "Home - Alpeche Pancha - Phixyn",
     "metadata" => array(
-        "site_description" => "Phixyn's personal website config file working",
-        "site_keywords" => "Phixyn, is, awesome config gile"
+        "site_description" => "Phixyn's personal website",
+        "site_keywords" => "Phixyn, is, awesome"
+    ),
+    "urls" => array(
+        "root" => $domain,
+        "pages" => $domain . "/pages",
+        "blog" => $domain . "/blog"
     ),
     "paths" => array(
-        "root" => __DIR__,
-        "css" => __DIR__ . DIRECTORY_SEPARATOR . "../css",
-        "js" => __DIR__ . DIRECTORY_SEPARATOR . "../js",
         "images" => array(
-            "root" => __DIR__ . DIRECTORY_SEPARATOR . "../img",
-            "social" => __DIR__ . DIRECTORY_SEPARATOR . "../img" . DIRECTORY_SEPARATOR . "social"
+            "root" => "/redhawk/img",
+            "social" => "/redhawk/social"
         ),
-        "fonts" => __DIR__ . DIRECTORY_SEPARATOR . "../fonts",
-        "templates" => __DIR__ . DIRECTORY_SEPARATOR . "../templates",
-        "pages" => __DIR__ . DIRECTORY_SEPARATOR . "../pages",
-        "blog" => __DIR__ . DIRECTORY_SEPARATOR . "../blog"
+        "css" => "/redhawk/css",
+        "js" => "/redhawk/js",
+        "fonts" => "/redhawk/fonts",
+        "includes" => array(
+            "templates" => __DIR__ . DIRECTORY_SEPARATOR . "../templates"
+        )
     )
 );
 
