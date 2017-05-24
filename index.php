30e11c4df124df98d27c0cba2cf0e561246ab8ef
phixyn <phixyn@gmail.com>
2017-05-24 22:25:03 +0100

    feat(templates-carousel): move carousel markup from index.php to templates/carousel.php

--- index.php
+++ index.php
@@ -1,59 +1,8 @@
 <?php
     require_once($_SERVER["DOCUMENT_ROOT"] . "/redhawk/phix.php");
     require_once($config["paths"]["includes"]["templates"] . "/header.php");
+    require_once($config["paths"]["includes"]["templates"] . "/carousel.php");
 ?>
-
-        <!-- Carousel -->
-        <div id="highlight-box" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-            <li data-target="#highlight-box" data-slide-to="0" class="active"></li>
-            <li data-target="#highlight-box" data-slide-to="1"></li>
-            <li data-target="#highlight-box" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-                <div class="item active">
-                    <img class="first-slide img-responsive" src="<?php echo $config['paths']['images']['root']; ?>/logo.png" alt="First slide">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>Watch me on Twitch!</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>Check out my Youtube</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>Catch me on Livecoding.tv</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <a class="left carousel-control" href="#highlight-box" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-
-            <a class="right carousel-control" href="#highlight-box" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div> <!-- /.carousel -->
 
         <!-- Main content -->
         <div class="container" id="main-content">
