0462fef94ccbce31ecd0b51532787a35bc5e7921
phixyn <phixyn@gmail.com>
2017-05-28 20:49:42 +0100

    feat(old-site-migration): update projects.php to use new PHP engine

--- pages/projects.php
+++ pages/projects.php
@@ -1,64 +1,47 @@
-<?php 
-	require_once($_SERVER["DOCUMENT_ROOT"] . "/redhawk/phix.php");
+<?php
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/redhawk/phix.php");
     require_once($config["paths"]["includes"]["templates"] . "/header.php");
 ?>
 
-<div class="body-wrapper" id="body-wrapper-800px">
+		<div class="banner"> Projects </div>
 
-	<div class="banner" id="banner-800px"> Projects </div> <!-- .banner#banner-800px -->
-	
-	<div class="main-content" id="content-800px">
-	
-		<h1> Portfolio </h1>
-		<div id="index-projects">
-			
-			<div class="index-project">
-				<img src="#" />
-				<div class="index-project-desc">
-					<h3> ZoeyBot </h3>
-					<p> IRC and Twitch.tv bot written in Python. </p>
-					<p><a href="#"> View details &raquo; </a></p>
-				</div> <!-- .index-project-desc -->
-			</div> <!-- .index-project -->
+        <!-- Main content -->
+        <div class="container" id="main-content">
+            <div class="row">
+                <h1> Portfolio </h1>
+                <div class="col-sm-4 col-md-4">
+                    <div class="thumbnail">
+                        <img src="<?php echo $config['paths']['images']['root']; ?>/gameplay3.png" alt="...">
+                        <div class="caption">
+                            <h3>TetrisPhix</h3>
+                            <p>Tetris clone written in Java using the AWT and Swing libraries.</p>
+                           <p><a href="#" class="btn btn-default" role="button">View details</a></p>
+                        </div>
+                    </div>
+                </div>
 
-			<div class="index-project">
-				<img src="#" />
-				<div class="index-project-desc">
-					<h3> StarPhix </h3>
-					<p> 2D retro space arcade game, written in Python using the pyGame module. </p>
-					<p><a href="#"> View details &raquo; </a></p>
-				</div> <!-- .index-project-desc -->
-			</div> <!-- .index-project -->
-			
-			<div class="index-project">
-				<img src="#" />
-				<div class="index-project-desc">
-					<h3> EpicPhix </h3>
-					<p> This is a description </p>
-					<p><a href="#"> View details &raquo; </a></p>
-				</div> <!-- .index-project-desc -->
-			</div> <!-- .index-project -->
-			
-		</div> <!-- #index-projects -->
-		
-		<div class="parent-clear"></div>	<!-- make parent element automatically expand to child elements' heights -->
-		
-	</div>	<!-- .main-content#content-800px -->
-	<div class="parent-clear"></div>	<!-- make parent element automatically expand to child elements' heights -->
-</div>	<!-- .body-wrapper#body-wrapper-800px -->
+                <div class="col-sm-4 col-md-4">
+                    <div class="thumbnail">
+                        <img src="<?php echo $config['paths']['images']['root']; ?>/colorPulse_V4.jpg" alt="...">
+                        <div class="caption">
+                            <h3>ZoeyBot</h3>
+                            <p>IRC and Twitch.tv bot written in Python.</p>
+					        <p><a href="#" class="btn btn-default" role="button">View details</a></p>
+                        </div>
+                    </div>
+                </div>
 
-<div class="body-wrapper" id="body-wrapper-320px">
-
-	<div class="banner" id="banner-320px"> Projects </div> <!-- .banner#banner-320px -->
-
-	<div class="main-content" id="content-320px">
-		<p> This is my mobile portfolio. You can view the full site by <a href="#">clicking here!</a>
-		<h1> Portfolio </h1>
-		<p style="text-align: center"><img src="#" />
-		<img src="#" />
-		<img src="#" /></p>
-	</div>	<!-- .main-content#content-320px -->
-
-</div>	<!-- .body-wrapper#body-wrapper-320px -->
+                <div class="col-sm-4 col-md-4">
+                    <div class="thumbnail">
+                        <img src="<?php echo $config['paths']['images']['root']; ?>/starphix.png" alt="...">
+                        <div class="caption">
+                            <h3>StarPhix</h3>
+                            <p>Retro space arcade game, written in Python using the pyGame module.</p>
+                            <p><a href="#" class="btn btn-default" role="button">View details</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.row -->
+        </div> <!-- /.container #main-content -->
 
 <?php require_once($config["paths"]["includes"]["templates"] . "/footer.php"); ?>
