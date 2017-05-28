bf7e1c1fe6447b10ae41a75a0ad79a150d3c2828
phixyn <phixyn@gmail.com>
2017-05-28 20:43:59 +0100

    fix col-sm values for projects

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
                     <h1>Hello! My name is Phixyn</h1>
                     <p> ...and I like to make videogames. During the day, I live life as a <a href="#">Computer Science</a> student at the University of Essex, in England. Later on in the day, you might catch me frivolously coding in my bedroom and <a href="#">tweeting</a> about my coding adventures. At night, I play games while watching live <a href="#">streams</a> of other people who also happen to play games. Welcome to my life! </p>
                     <p> Don't forget to change box-shadows to rgb()! </p>
-                </div> <!-- /#content-intro -->
+                </div>
 
                 <div class="col-sm-6 col-md-2">
                     <img src="<?php echo $config['paths']['images']['root']; ?>/me3.png" style="max-width: 226px; max-height: 218px;" />
                 </div>
-            </div> <!-- /.row -->
+            </div> <!-- /.row /#content-intro -->
 
             <div class="row">
                 <h1> My projects </h1>
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-4 col-md-4">
                     <div class="thumbnail">
                         <img src="<?php echo $config['paths']['images']['root']; ?>/gameplay3.png" alt="...">
                         <div class="caption">
@@ -35,7 +35,7 @@
                     </div>
                 </div>
 
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-4 col-md-4">
                     <div class="thumbnail">
                         <img src="<?php echo $config['paths']['images']['root']; ?>/colorPulse_V4.jpg" alt="...">
                         <div class="caption">
@@ -46,7 +46,7 @@
                     </div>
                 </div>
 
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-4 col-md-4">
                     <div class="thumbnail">
                         <img src="<?php echo $config['paths']['images']['root']; ?>/starphix.png" alt="...">
                         <div class="caption">
