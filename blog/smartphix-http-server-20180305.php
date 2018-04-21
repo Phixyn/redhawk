602aa9c10f9f23e7c3cb882f9d60a5300ae408e9
phixyn <phixyn@gmail.com>
2018-04-21 13:26:04 +0100

    feat(blog): update Smartphix HTTP server blog post and add it to to index pages of blog and tags/smartphix

--- blog/smartphix-http-server-20180305.php
+++ blog/smartphix-http-server-20180305.php
@@ -11,14 +11,14 @@
             <div class="row blog-post">
 				<div class="blog-header">
 					<div class="blog-title">SmartPhix HTTP Server</div>
-					<div class="blog-meta">Posted on March 5, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
+					<div class="blog-meta">Posted on March 6th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
                 <a href="<?php echo $config['urls']['root']; ?>/static/blog/image-uploads/communication-1927706_640.jpg" target="_blank"><img src="<?php echo $config['urls']['root']; ?>/static/blog/image-uploads/communication-1927706_640.jpg" alt="Internet of Things" style="width: 40%;height:50%;" /></a>
 
-                <p>Last week, I discussed a <a href="http://flask.pocoo.org/" target="_blank">new project idea</a> I had involving Phidgets and a companion app. I started looking at the backend for the project, mainly writing the writing the HTTP server in Python. As with all of my newer projects, I am using Python 3 for this, as more systems are making the move to this version, with the biggest exception being legacy systems.</p>
+                <p>Last week, I discussed a <a href="<?php echo $config['urls']['blog']; ?>/smartphix-home-automation-20180220.php" target="_blank">new project idea</a> I had involving Phidgets and a companion app. I started looking at the backend for the project, mainly writing the HTTP server in Python. As with all of my newer projects, I am using Python 3 for this, as more systems are making the move to this version, with the biggest exception being legacy systems.</p>
 
-                <p>For the HTTP server, I chose to use the Python BaseHTTPServer class from the http.server module. My other options were to use a more defined and flexible library such as <a href="<?php echo $config['urls']['blog']; ?>/smartphix-home-automation-20180220.php" target="_blank">Flask</a>. However, I felt that with such a simple API, it would not benefit me greatly to use Flask or any other 3rd party library over what comes shipped with Python. This should also make it easier to set the project up on multiple systems, such as my Arch Linux Raspberry Pi.</p>
+                <p>For the HTTP server, I chose to use the Python BaseHTTPServer class from the http.server module. My other options were to use a more defined and flexible library such as <a href="http://flask.pocoo.org/" target="_blank">Flask</a>. However, I felt that with such a simple API, it would not benefit me greatly to use Flask or any other 3rd party library over what comes shipped with Python. This should also make it easier to set the project up on multiple systems, such as my Arch Linux Raspberry Pi.</p>
 
                 <p>I was able to make a server that listened for requests and logged them fairly quickly, writing a subclass of BaseHTTPRequestHandler to handle the API requests. The next challenge I had was to integrate the Phidgets' API into the server, in order to have the server return the temperature when a GET request to /temperature is made. Unfortunately, this is less simple than the first part.</p>
 
--- blog/tags/smartphix/index.php
+++ blog/tags/smartphix/index.php
@@ -10,6 +10,21 @@
         <div class="container" id="main-content">
 			<div class="row blog-post">
 				<div class="blog-header">
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server-20180305.php" title="SmartPhix HTTP Server">SmartPhix HTTP Server</a></div>
+					<div class="blog-meta">Posted on March 6th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
+				</div>	<!-- .blog-header -->
+
+				<a href="<?php echo $config['urls']['root']; ?>/static/blog/image-uploads/communication-1927706_640.jpg" target="_blank"><img src="<?php echo $config['urls']['root']; ?>/static/blog/image-uploads/communication-1927706_640.jpg" alt="Internet of Things" style="width: 40%;height:50%;" /></a>
+
+				<p>Last week, I discussed a <a href="<?php echo $config['urls']['blog']; ?>/smartphix-home-automation-20180220.php" target="_blank">new project idea</a> I had involving Phidgets and a companion app. I started looking at the backend for the project, mainly writing the HTTP server in Python. As with all of my newer projects, I am using Python 3 for this, as more systems are making the move to this version, with the biggest exception being legacy systems.</p>
+
+				<p>For the HTTP server, I chose to use the Python BaseHTTPServer class from the http.server module. My other options were to use a more defined and flexible library such as <a href="http://flask.pocoo.org/" target="_blank">Flask</a>. However, I felt that with such a simple API, it would not benefit me greatly to use Flask or any other 3rd party library over what comes shipped with Python. This should also make it easier to set the project up on multiple systems, such as my Arch Linux Raspberry Pi.</p>
+
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server-20180305.php" title="Continue reading">Continue reading &raquo;</a></p>
+			</div> <!-- /.row /.blog-post -->
+
+			<div class="row blog-post">
+				<div class="blog-header">
 					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-home-automation-20180220.php" title="SmartPhix - A home automation project">SmartPhix - A home automation project</a></div>
 					<div class="blog-meta">Posted on February 20, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
