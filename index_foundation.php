295b17fa682354f96f7a8c65a8b84c24646283bd
phixyn <phixyn@gmail.com>
2019-12-21 18:20:48 +0000

    :package: add foundation orbit template file
    Adds a new PHP template file for the orbit component of the
    website. Includes the orbit template in the index page.

--- index_foundation.php
+++ index_foundation.php
@@ -4,52 +4,8 @@
     $config['page_title'] = "Home - Alpeche Pancha - Phixyn";
     // TODO test overriding metadata
     require_once($config["paths"]["includes"]["templates"] . "/header_foundation.php");
-    // require_once($config["paths"]["includes"]["templates"] . "/orbit_foundation.php");
+    require_once($config["paths"]["includes"]["templates"] . "/orbit_foundation.php");
 ?>
-
-    <!-- ORBIT -->
-    <div class="orbit" id="home-orbit" role="region" aria-label="Featured" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
-      <div class="orbit-wrapper">
-        <div class="orbit-controls">
-          <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-          <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-        </div> <!-- .orbit-controls -->
-        <ul class="orbit-container" id="home-orbit-container">
-          <li class="is-active orbit-slide">
-            <figure class="orbit-figure">
-              <img class="orbit-image" src="https://foundation.zurb.com/sites/docs/assets/img/orbit/01.jpg" alt="Space" />
-              <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-            </figure>
-          </li>
-          <li class="orbit-slide">
-            <figure class="orbit-figure">
-              <img class="orbit-image" src="https://foundation.zurb.com/sites/docs/assets/img/orbit/02.jpg" alt="Space" />
-              <figcaption class="orbit-caption">Lets Rocket!</figcaption>
-            </figure>
-          </li>
-          <li class="orbit-slide">
-            <figure class="orbit-figure">
-              <img class="orbit-image" src="https://foundation.zurb.com/sites/docs/assets/img/orbit/03.jpg" alt="Space" />
-              <figcaption class="orbit-caption">Encapsulating</figcaption>
-            </figure>
-          </li>
-          <li class="orbit-slide">
-            <figure class="orbit-figure">
-              <img class="orbit-image" src="https://foundation.zurb.com/sites/docs/assets/img/orbit/04.jpg" alt="Space" />
-              <figcaption class="orbit-caption">Outta This World</figcaption>
-            </figure>
-          </li>
-        </ul> <!-- .orbit-container -->
-      </div> <!-- .orbit-wrapper -->
-
-      <!-- ORBIT NAV -->
-      <nav class="orbit-bullets">
-        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-      </nav>
-    </div> <!-- .orbit -->
 
     <!-- MAIN CONTENT -->
     <div class="grid-container">
