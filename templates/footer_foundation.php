3d125c450a060189263993537905efeb32d7aceb
phixyn <phixyn@gmail.com>
2019-12-22 16:02:07 +0000

    :package: display project version number in footer
    Updates the footer foundation PHP template to
    contain the project's version number from the
    config.

--- templates/footer_foundation.php
+++ templates/footer_foundation.php
@@ -3,7 +3,8 @@
     <div class="grid-container full" id="footer">
       <div class="grid-x">
         <div class="cell">
-          <p class="copyright-msg">&copy; <?php $fromYear = 2014; $thisYear = (int)date('Y'); echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : '');?> Alpeche Pancha. All rights reserved.</p>
+          <p class="copyright-msg">&copy; <?php $fromYear = 2014; $thisYear = (int)date('Y'); echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : '');?> Alpeche Pancha. All rights reserved. <br />
+          <?php echo $config['version'] ?></p>
         </div> <!-- .cell -->
       </div> <!-- .grid-x -->
     </div> <!-- .grid-container -->
