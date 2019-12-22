d8eed44dceffa883d4fba2c635d90f31cb889af7
phixyn <phixyn@gmail.com>
2019-12-22 16:05:56 +0000

    :sparkles: update links to projects and images
    Updates all hyperlinks to project pages and
    images to use values from the config file
    for easy maintainability.

--- index_foundation.php
+++ index_foundation.php
@@ -1,10 +1,10 @@
 <?php
-    require_once($_SERVER["DOCUMENT_ROOT"] . "/phix.php");
-    // Override title and metadata variables here before including header
-    $config['page_title'] = "Home - Alpeche Pancha - Phixyn";
-    // TODO test overriding metadata
-    require_once($config["paths"]["includes"]["templates"] . "/header_foundation.php");
-    require_once($config["paths"]["includes"]["templates"] . "/orbit_foundation.php");
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/phix.php");
+  // Override title and metadata variables here before including header
+  $config['page_title'] = "Home - Alpeche Pancha - Phixyn";
+  // TODO test overriding metadata
+  require_once($config["paths"]["includes"]["templates"] . "/header_foundation.php");
+  require_once($config["paths"]["includes"]["templates"] . "/orbit_foundation.php");
 ?>
 
     <!-- MAIN CONTENT -->
@@ -23,20 +23,20 @@
 
               <p>My name is Alpeche Pancha. I work full-time as a <a href="https://en.wikipedia.org/wiki/Test_engineer" title="Wikipedia - Test Engineer">Software Test Engineer</a> and study part-time for my <a href ="https://www.uos.ac.uk/courses/ug/bsc-hons-computer-games-programming" title="Computer Games Programming at UOS">BSc Computer Games Programming</a> degree. I am based in Suffolk, work at <a href="http://atadastral.co.uk/about/" title="Adastral Park">Adastral Park</a> and study at the <a href="https://www.uos.ac.uk/" title="University of Suffolk">University of Suffolk</a>. Currently, I am on my second year at University.</p>
 
-              <p>In my little spare time, I like to work on my personal projects, such as this website. I also play video games (of course) and play the bass guitar. On this website you can find out more about my hobbies and <a href="projects_foundation.html" title="Projects">projects.</a>
+              <p>In my little spare time, I like to work on my personal projects, such as this website. I also play video games (of course) and play the bass guitar. On this website you can find out more about my hobbies and <a href="<?php echo $config['urls']['pages']['root']; ?>/projects_foundation.php" title="Projects">projects.</a>
 
               <p>If you wish, you can drop a follow on my <a href="https://www.twitter.com/Phixyn" title="Twitter - Alpeche Pancha (@Phixyn)">Twitter.</a></p>
             </div> <!-- .media-object-section -->
 
             <div class="media-object-section">
-              <div class="thumbnail"><img src="img/me3.png" alt="Avatar" /></div>
+              <div class="thumbnail"><img src="<?php echo $config['paths']['images']['root']; ?>/me3.png" alt="Avatar" /></div>
             </div> <!-- .media-object-section -->
           </div> <!-- .media-object -->
         </div> <!-- .cell -->
 
         <div class="cell">
           <h2 class="h4">My projects</h2>
-          <p> These are just some of my game projects. You can view the rest in the <a href="projects_foundation.html" title="Projects">projects page.</a></p>
+          <p> These are just some of my game projects. You can view the rest in the <a href="<?php echo $config['urls']['pages']['root']; ?>/projects_foundation.php" title="Projects">projects page.</a></p>
         </div> <!-- .cell -->
 
         <div class="cell large-4 medium-4">
@@ -45,7 +45,7 @@
               <h3 class="h5">MineCart</h3>
             </div> <!-- .card-divider -->
 
-            <img src="img/mc_1.PNG" alt="MineCart" />
+            <img src="<?php echo $config['paths']['images']['root']; ?>/mc_1.PNG" alt="MineCart" />
 
             <div class="card-section">
               <span class="label success">Completed</span>
@@ -56,7 +56,7 @@
             </div> <!-- .card-section -->
 
             <div class="card-section">
-              <a href="minecart.html" class="button">View Details</a>
+              <a href="<?php echo $config['urls']['pages']['projects']; ?>/minecart.php" class="button">View Details</a>
             </div> <!-- .card-section -->
           </div> <!-- .card -->
         </div> <!-- .cell -->
@@ -67,7 +67,7 @@
               <h3 class="h5">PhinyxEngine</h3>
             </div> <!-- .card-divider -->
 
-            <img src="img/pe_3.PNG" alt="PhinyxEngine" />
+            <img src="<?php echo $config['paths']['images']['root']; ?>/pe_3.PNG" alt="PhinyxEngine" />
 
             <div class="card-section">
               <span class="label primary">In development</span>
@@ -78,7 +78,7 @@
             </div> <!-- .card-section -->
 
             <div class="card-section">
-              <a href="phinyxengine.html" class="button">View Details</a>
+              <a href="<?php echo $config['urls']['pages']['projects']; ?>/phinyxengine.php" class="button">View Details</a>
             </div> <!-- .card-section -->
           </div> <!-- .card -->
         </div> <!-- .cell -->
@@ -89,7 +89,7 @@
               <h3 class="h5">TetrisPhix</h3>
             </div> <!-- .card-divider -->
 
-            <img src="img/gameplay3.png" alt="TetrisPhix" />
+            <img src="<?php echo $config['paths']['images']['root']; ?>/gameplay3.png" alt="TetrisPhix" />
 
             <div class="card-section">
               <span class="label success">Completed</span>
@@ -100,7 +100,7 @@
             </div> <!-- .card-section -->
 
             <div class="card-section">
-              <a href="tetrisphix.html" class="button">View Details</a>
+              <a href="<?php echo $config['urls']['pages']['projects']; ?>/tetrisphix.php" class="button">View Details</a>
             </div> <!-- .card-section -->
           </div> <!-- .card -->
         </div> <!-- .cell -->
