9116b5afe768e3a11cdc223630b6cfcf8eb5b12e
phixyn <phixyn@gmail.com>
2020-01-05 01:30:02 +0000

    :refactor: rename Bootstrap blog pages
    Renames all blog posts using the Bootstrap version
    of the site. These will eventually be deleted when
    the migration to Foundation is fully complete.

--- blog/index_bootstrap.php
+++ blog/index_bootstrap.php
@@ -11,18 +11,18 @@
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/welcome-to-redhawk-20171111.php" title="Welcome to the website">Welcome To The Website</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/welcome-to-redhawk-20171111_bootstrap.php" title="Welcome to the website">Welcome To The Website</a></div>
 					<div class="blog-meta">Posted on November 11, 2017 in <a href="#">Uncategorized</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
 				<p>Welcome to my website! This is the first iteration of my website, after a complete refactor and some re-design. It is very much a work in progress. I aim to release a new iteration at least every two weeks, but this may vary depending on how busy I am with coursework and my full-time job.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/welcome-to-redhawk-20171111.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/welcome-to-redhawk-20171111_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
             </div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/v020-release-notes-20171111.php" title="v0.2.0 release notes">v0.2.0 Release Notes</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/v020-release-notes-20171111_bootstrap.php" title="v0.2.0 release notes">v0.2.0 Release Notes</a></div>
 					<div class="blog-meta">Posted on November 11, 2017 in <a href="#">Uncategorized</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -34,18 +34,18 @@
                     <li>Added a "carousel" section to the homepage</li>
                 </ul>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/v020-release-notes-20171111.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/v020-release-notes-20171111_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
             </div> <!-- /.row /.blog-post -->
 
             <div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/new-years-resolutions-20141225.php" title="On New Years' resolutions">On New Years' Resolutions</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/new-years-resolutions-20141225_bootstrap.php" title="On New Years' resolutions">On New Years' Resolutions</a></div>
 					<div class="blog-meta">Posted on December 25, 2014 in <a href="#">Uncategorized</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
 				<p>A new year is quickly approaching and with this, people are starting to jot down their plans and goals for the year ahead. New years' resolutions are a great way to motivate yourself and get hyped. However, they might not always work and each year, you might end up with a few goals and plans left unattended, which can make you feel rather disappointed in yourself.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/new-years-resolutions-20141225.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/new-years-resolutions-20141225_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
             </div> <!-- /.row /.blog-post -->
 
         </div> <!-- /.container #main-content -->
--- blog/tags/smartphix/index.php
+++ blog/tags/smartphix/index.php
@@ -10,7 +10,7 @@
         <div class="container" id="main-content">
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-and-gaming.php" title="SmartPhix">SmartPhix - The IoT and Gaming</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-and-gaming_bootstrap.php" title="SmartPhix">SmartPhix - The IoT and Gaming</a></div>
 					<div class="blog-meta">Posted on February 20th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -18,12 +18,12 @@
 
 <p>My project, called SmartPhix, will explore the Internet of Things (IoT) using smart home devices. My aim is to investigate how video games and software applications can make use of IoT technology to enhance user experience.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-and-gaming.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-and-gaming_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-use-case.php" title="SmartPhix">SmartPhix - A use case for IoT in game development</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-use-case_bootstrap.php" title="SmartPhix">SmartPhix - A use case for IoT in game development</a></div>
 					<div class="blog-meta">Posted on February 27th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -33,12 +33,12 @@
 
 <p>In the past, game developers were limited to the game itself when it came to providing immersion to players. Over time, developers gained new ways to deliver output from in-game events, such as vibration and LED lights in controllers. PC gaming peripherals also offer the ability to control their LED lights based on in-game events. For example, the RGB LED colors in Razer Chroma peripherals can be programmatically via the Chroma SDK, which can be used to develop Chroma apps. Many official game integrations, such as Overwatch’s Chroma App, are readily available and come pre-installed with the game itself.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-use-case.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-use-case_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-project.php" title="SmartPhix">SmartPhix - A simple IoT project</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-project_bootstrap.php" title="SmartPhix">SmartPhix - A simple IoT project</a></div>
 					<div class="blog-meta">Posted on March 6th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -46,12 +46,12 @@
 
 <p>In this project, I will explore the Philips Hue smart LED lights, and I will be using the Hue Bridge and its API to change the light’s settings based on environment change. However, I will not be developing a game to demonstrate the concept, as that could take a long time and the key principles of this concept could be lost in the complexity of the game’s development. Instead, I will  develop a very simple smart home IoT project to monitor the temperature inside my house and adjust my Philips Hue light color based on the current temperature. In a video game, a change in game scenery (e.g. time of day or weather) could act as a trigger for changing the Hue light. But to demonstrate this concept, we can do something as simple as monitoring the temperature of a room and have the Hue light react accordingly.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-project.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-iot-project_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server.php" title="SmartPhix">SmartPhix - Phidget API and HTTP Server</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server_bootstrap.php" title="SmartPhix">SmartPhix - Phidget API and HTTP Server</a></div>
 					<div class="blog-meta">Posted on March 13th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -59,12 +59,12 @@
 
 <p>For the HTTP server, I chose to use the Python BaseHTTPServer class from the http.server module. My other options were to use a more defined and flexible library such as Flask. However, I felt that with such a simple API, it would not benefit me greatly to use Flask or any other 3rd party library over what is already included in the Python standard libraries. This should also make it easier to set the project up on multiple systems, such as a Raspberry Pi running a Linux distro.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-http-server_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-server-updates.php" title="SmartPhix">SmartPhix - HTTP Server Updates</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-server-updates_bootstrap.php" title="SmartPhix">SmartPhix - HTTP Server Updates</a></div>
 					<div class="blog-meta">Posted on March 23rd, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -72,12 +72,12 @@
 
 <p>In order solve this, I had to create a subclass of HTTPServer, which I appropriately named SmartphixHTTPServer. I then created and stored an instance of my PhidgetHelper class in a member variable. The request handler classes were then able to access this member variable by referencing the server object (i.e. self.server.member_var), which is an instance variable of BaseHTTPRequestHandler. This variable seems to be instantiated when our HTTP request handler <b>sub-class (not object, the class reference itself)</b> is passed as an argument of the HTTPServer class’ constructor method.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-server-updates.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-server-updates_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-and-retrofit.php" title="SmartPhix">SmartPhix - Android App and Retrofit</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-and-retrofit_bootstrap.php" title="SmartPhix">SmartPhix - Android App and Retrofit</a></div>
 					<div class="blog-meta">Posted on March 30th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -87,12 +87,12 @@
 
 <p>By default, Android studio projects use Gradle to manage dependencies, such as extra libraries needed for the project. To make my project use the Retrofit API libraries, all I had to do was to update the app-level “build.gradle” file to specify that I wanted Retrofit in my depedencies. Since Retrofit makes HTTP requests, it needs permission to use the network connection on the Android device. This can be specified in the AndroidManifest.xml file, with the XML tag "uses-permission".</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-and-retrofit.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-and-retrofit_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-updates.php" title="SmartPhix">SmartPhix - Android App Updates</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-updates_bootstrap.php" title="SmartPhix">SmartPhix - Android App Updates</a></div>
 					<div class="blog-meta">Posted on April 6th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -100,12 +100,12 @@
 
 <p>To display time and date, I replaced the TextView widgets with TextClock widget, which automatically handles displaying the current time and date without any additional code. Initially, I thought that the TextClock widget only displayed time data. However, while learning about the various ways to format the time in 12-hour and 24-hour formats, I stumbled upon the <a href="https://developer.android.com/reference/java/text/SimpleDateFormat#date-and-time-patterns">SimpleDateFormat</a> patterns. Out of curiosity, I tried to use date specific formatters in a TextClock widget to see if I could display the current date using it. To my surprise it worked.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-updates.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-android-app-updates_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-parsing-json-in-app.php" title="SmartPhix">SmartPhix - Parsing JSON data in the app</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-parsing-json-in-app_bootstrap.php" title="SmartPhix">SmartPhix - Parsing JSON data in the app</a></div>
 					<div class="blog-meta">Posted on April 13th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
@@ -113,18 +113,18 @@
 
 <p>To parse plain text data from my server, I used the Scalar converter, which simply takes the text and converts it to a String object. A Gson converter is needed for JSON data. I can specify the type of converter to use when creating my Retrofit object in the MainActivity class. The first step was to replace the Scalar converter with Gson.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-parsing-json-in-app.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-parsing-json-in-app_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
 
 			<div class="row blog-post">
 				<div class="blog-header">
-					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-conclusion.php" title="SmartPhix">SmartPhix - Conclusions and future plans</a></div>
+					<div class="blog-title"><a href="<?php echo $config['urls']['blog']; ?>/smartphix-conclusion_bootstrap.php" title="SmartPhix">SmartPhix - Conclusions and future plans</a></div>
 					<div class="blog-meta">Posted on April 27th, 2018 in <a href="<?php echo $config['urls']['blog']; ?>/tags/smartphix/index.php">SmartPhix</a> &nbsp; | &nbsp; Comments: Off</div>
 				</div>	<!-- .blog-header -->
 
 				<p>Generally, I am happy with my prototype app for the Smartphix project, but there is a lot to be improved. It is currently not practical to use, as IP addresses and API tokens for the Hue API have to be hardcoded both in the app and server. Furthermore, the app’s MainActivity class can be improved to boost performance and usability. Currently, the user has to tap the “Update” button to get new temperatures and set their Hue’s color. However, it would be more practical to have the app update at a set interval, which could be configured by the user. Additionally, the app UI should provide a way to specify the IP addresses of the HTTP server and Hue Bridge.</p>
 
-				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-conclusion.php" title="Continue reading">Continue reading &raquo;</a></p>
+				<p><a href="<?php echo $config['urls']['blog']; ?>/smartphix-conclusion_bootstrap.php" title="Continue reading">Continue reading &raquo;</a></p>
 			</div> <!-- /.row /.blog-post -->
         </div> <!-- /.container #main-content -->
 
