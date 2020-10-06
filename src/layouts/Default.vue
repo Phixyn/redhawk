2dbcbf89d50c3c4d6d30610996542ff7a3bb08aa
phixyn <phixyn@gmail.com>
2020-10-06 02:11:18 +0100

    Pages: Add new pages for projects and contact
    Add the "Projects" and "Contact" pages to the Gridsome site. Update the
    navigation links in the default layout to include links to these new pages.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,7 +6,9 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/projects/">Projects</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/contact/">Contact</g-link>
       </nav>
     </header>
     <slot/>
---
+++ src/pages/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Projects</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt adipisci qui porro nisi nulla libero! Quas voluptatibus odio similique, qui ullam laudantium. Iusto cum explicabo atque voluptatem consectetur dignissimos beatae perspiciatis saepe vero alias quae, iste nesciunt quas perferendis veritatis repellendus ipsum animi debitis aut similique at, dolore incidunt.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Projects"
+  }
+}
+</script>
