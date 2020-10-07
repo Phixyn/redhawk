2febaa80d806f0d907bc55b1540131c6288ef952
phixyn <phixyn@gmail.com>
2020-10-07 08:53:23 +0100

    Layout: Remove unused code from default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,5 @@
 <template>
   <div>
-    <!-- <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/projects/">Projects</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-        <g-link class="nav__link" to="/contact/">Contact</g-link>
-      </nav>
-    </header> -->
     <Header />
     <slot/>
   </div>
@@ -33,31 +22,3 @@ export default {
   }
 }
 </script>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
