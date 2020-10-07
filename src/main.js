755fb5334c6f5289fad0fa643ffc388283258849
phixyn <phixyn@gmail.com>
2020-10-07 06:55:59 +0100

    Buefy: Start using our own scss file to customize Buefy
    Add a new SCSS file to a folder called assets. Import this file in main.js.
    Remove the old import for Buefy. I don't think it's needed (docs are
    confusing) because we now have `@import "~buefy/src/scss/buefy";` in the
    SCSS file, so it should be fine to remove it?

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import Buefy from 'buefy'
+import DefaultLayout from "~/layouts/Default.vue";
+import Buefy from "buefy";
 
-import 'buefy/dist/buefy.css'
+// import 'buefy/dist/buefy.css'
+import "~/assets/styles.scss";
 
-export default function (Vue, { router, head, isClient }) {
-  Vue.use(Buefy)
+export default function(Vue, { router, head, isClient }) {
+  Vue.use(Buefy);
 
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
 }
