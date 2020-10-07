381d902d210c471ae21e3f4c43cd2afb54534120
phixyn <phixyn@gmail.com>
2020-10-07 09:43:30 +0100

    Index: Query for projects and display project previews component

--- src/pages/IndexV2.vue
+++ src/pages/IndexV2.vue
@@ -4,25 +4,44 @@
       <ContentBanner v-bind:banner="$page.customPage.banner"/>
       <div class="container" v-html="$page.customPage.content"></div>
     </section>
+    <section>
+      <ProjectPreview v-bind:projects="$page.projects"/>
+    </section>
   </Layout>
 </template>
 
 <page-query>
-query HomePage {
+query {
   customPage (path: "/") {
     title
     banner
     content
+  }
+
+  projects: allProject {
+    edges {
+      node {
+        id
+        path
+        content
+        excerpt
+        title
+        banner
+        image_path
+      }
+    }
   }
 }
 </page-query>
 
 <script>
 import ContentBanner from "@/components/ContentBanner.vue";
+import ProjectPreview from "@/components/ProjectPreview.vue";
 
 export default {
   components: {
-    ContentBanner
+    ContentBanner,
+    ProjectPreview
   },
   metaInfo() {
     return {
