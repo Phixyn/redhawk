827d01f6992611648a4d85fb2afe789bbfe77a97
phixyn <phixyn@gmail.com>
2020-10-07 08:54:08 +0100

    App: Remove unused code and re-order imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from "~/layouts/Default.vue";
 import Buefy from "buefy";
+import DefaultLayout from "~/layouts/Default.vue";
 
-// import 'buefy/dist/buefy.css'
 import "~/assets/styles.scss";
 
 export default function(Vue, { router, head, isClient }) {
