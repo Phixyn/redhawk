789d5d0ac54cd7e33d6ce39989e17cc8e3a6a4eb
phixyn <phixyn@gmail.com>
2020-10-07 05:49:06 +0100

    Chore: Add Buefy to the project
    Add the Buefy plugin, a CSS framework based on Bulma for Vue.js.
    Register the plugin usage in the main.js file by importing it and
    using Vue.use().

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Buefy from 'buefy'
+
+import 'buefy/dist/buefy.css'
 
 export default function (Vue, { router, head, isClient }) {
+  Vue.use(Buefy)
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
