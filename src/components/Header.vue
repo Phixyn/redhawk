460f646f78ca44fbd94459610371a41a7ad95ca1
phixyn <phixyn@gmail.com>
2020-10-07 08:52:25 +0100

    Header: Fix warnings and update formatting
    Instead of trying to use string values for props, use booleans correctly.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-navbar type="is-black" shadow="true">
+  <b-navbar class="mb-5" type="is-black" shadow>
     <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <b-navbar-item tag="g-link" :to="{ path: '/' }">
         <img
           src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
           alt="Lightweight UI components for Vue.js based on Bulma"
@@ -14,17 +14,23 @@
         tag - Set tag type to <g-link> to make sure Gridsome routing works
         to - part of <g-link> tag, like href but for routing
       -->
-      <b-navbar-item tag="g-link" to="/"> Home </b-navbar-item>
-      <b-navbar-item tag="g-link" to="/projects/"> Projects </b-navbar-item>
-      <b-navbar-item tag="g-link" to="/about/"> About </b-navbar-item>
-      <b-navbar-item tag="g-link" to="/contact/"> Contact </b-navbar-item>
+      <b-navbar-item tag="g-link" :to="{ path: '/' }"> Home </b-navbar-item>
+      <b-navbar-item tag="g-link" :to="{ path: '/projects/' }">
+        Projects
+      </b-navbar-item>
+      <b-navbar-item tag="g-link" :to="{ path: '/about/' }">
+        About
+      </b-navbar-item>
+      <b-navbar-item tag="g-link" :to="{ path: '/contact/' }">
+        Contact
+      </b-navbar-item>
       <!--
         hoverable - Users can hover to see dropdown items
         right - Anchor dropdown to right-side, should be true for dropdowns
           used at "end"
         collapsible - Make dropdown collapsible on mobile/small screens
       -->
-      <b-navbar-dropdown label="Social Media" hoverable="true" right="true" collapsible="true">
+      <b-navbar-dropdown label="Social Media" hoverable right collapsible>
         <b-navbar-item href="#"> About </b-navbar-item>
         <b-navbar-item href="#"> Contact </b-navbar-item>
       </b-navbar-dropdown>
