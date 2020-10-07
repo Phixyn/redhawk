5b77a2c5a87fde404dfe352ad9c860d4326c3a4a
phixyn <phixyn@gmail.com>
2020-10-07 08:57:53 +0100

    Pages: Add a new WIP index page
    Instead of having a single template for all pages, each page should have its
    own file in the pages/ folder. This will give us more control over how to
    display different parts of it. This commit adds a WIP page to replace the
    index page.

---
+++ src/pages/IndexV2.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <section class="section">
+      <ContentBanner v-bind:banner="$page.customPage.banner"/>
+      <div class="container" v-html="$page.customPage.content"></div>
+    </section>
+  </Layout>
+</template>
+
+<page-query>
+query HomePage {
+  customPage (path: "/") {
+    title
+    banner
+    content
+  }
+}
+</page-query>
+
+<script>
+import ContentBanner from "@/components/ContentBanner.vue";
+
+export default {
+  components: {
+    ContentBanner
+  },
+  metaInfo() {
+    return {
+      title: this.$page.customPage.title,
+    };
+  },
+};
+</script>
