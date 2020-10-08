49f544b8808ebbf264c4c241a80eeba3c66bb1d5
phixyn <phixyn@gmail.com>
2020-10-08 06:17:39 +0100

    Components: Improve HTML and responsiveness of project preview
    Update the project preview component to remove a redundant div element. This
    also fixes an issue with Bulma columns not working correctly. Also makes the
    columns work on mobile and gives them a fixed size.

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="columns">
-    <div v-bind:key="projectEdge.node.id" v-for="projectEdge in projects.edges">
-      <div class="column">
-        <ProjectCard v-bind:project="projectEdge.node" />
-      </div>
+  <!-- TODO not sure if I like is-mobile here...
+    prefer stacked on mobile maybe -->
+  <div class="columns is-mobile is-multiline">
+    <div
+      class="column is-4"
+      v-bind:key="projectEdge.node.id"
+      v-for="projectEdge in projects.edges"
+    >
+      <ProjectCard v-bind:project="projectEdge.node" />
     </div>
   </div>
 </template>
