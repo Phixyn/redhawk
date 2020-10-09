ebff7d45871cf21236f586d6a2884584964be137
phixyn <phixyn@gmail.com>
2020-10-09 14:37:39 +0100

    Layout: Remove unused query in Default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,14 +6,6 @@
   </div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
-
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
