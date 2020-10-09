42fe0710dd8118d242d87f6377ccedccdf07a6a2
phixyn <phixyn@gmail.com>
2020-10-09 09:14:49 +0100

    Header: Fix broken hoverable item
    The "hoverable" attribute does not work when combined with the "collapsible"
    attribute. The latter has been removed.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,6 +16,7 @@
         tag - Set tag type to <g-link> to make sure Gridsome routing works
         to - part of <g-link> tag, like href but for routing
       -->
+      <!-- TODO Use 'active' property and a class to style active items -->
       <b-navbar-item tag="g-link" :to="{ path: '/' }"> Home </b-navbar-item>
       <b-navbar-item tag="g-link" :to="{ path: '/projects/' }">
         Projects
@@ -30,12 +31,12 @@
         Contact
       </b-navbar-item>
       <!--
-        hoverable - Users can hover to see dropdown items
+        hoverable - Users can hover to see dropdown items (doesn't work with
+          collapsible attribute)
         right - Anchor dropdown to right-side, should be true for dropdowns
           used at "end"
-        collapsible - Make dropdown collapsible on mobile/small screens
       -->
-      <b-navbar-dropdown label="Social Media" hoverable right collapsible>
+      <b-navbar-dropdown label="Social Media" hoverable right arrowless>
         <b-navbar-item href="#"> GitHub </b-navbar-item>
         <b-navbar-item href="#"> Twitter </b-navbar-item>
         <b-navbar-item href="#"> LinkedIn </b-navbar-item>
