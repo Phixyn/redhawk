34383578c253751a89163440548a7b36c8c591fa
phixyn <phixyn@gmail.com>
2020-10-10 17:25:04 +0100

    Gridsome-Config: Load global sass/scss preprocessor files
    Make global SASS/SCSS assets available throughout the app. This allows us to
    use global sass variables, mixins and other stuff throughout the app without
    having to import these individually all the time.
    
    For more info, see:
    https://gridsome.org/docs/assets-css/#global-preprocessor-files-ie-variables-mixins

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,40 @@
 
 // Changes here require a server restart.
 
+const path = require("path");
+
+// Load global sass/scss pre-processor files. See:
+// https://gridsome.org/docs/assets-css/#global-preprocessor-files-ie-variables-mixins
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resources-loader")
+    .options({
+      patterns: [
+        // Be careful to use the correct path(s)
+        // path.resolve(__dirname, './src/assets/sass/*.sass'),
+        // path.resolve(__dirname, './src/assets/sass/_globals.scss'),
+        // path.resolve(__dirname, './src/assets/sass/_globals.scss'),
+        path.resolve(__dirname, "./src/assets/*.scss"),
+      ],
+    });
+}
+
 module.exports = {
   siteName: "Redhawk",
+  chainWebpack(config) {
+    // https://gridsome.org/docs/assets-css/#global-preprocessor-files-ie-variables-mixins
+    // Load variables for all vue-files
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+
+    // types.forEach(type => {
+    //   addStyleResource(config.module.rule('sass').oneOf(type))
+    // })
+
+    types.forEach((type) => {
+      addStyleResource(config.module.rule("scss").oneOf(type));
+    });
+  },
   plugins: [
     {
       use: "@gridsome/source-filesystem",
@@ -13,7 +45,7 @@ module.exports = {
         path: "./temp-content/pages/*.md",
         remark: {
           // Remark options
-        }
+        },
       },
     },
     {
@@ -23,13 +55,13 @@ module.exports = {
         path: "./temp-content/projects/*.md",
         remark: {
           // Remark options
-        }
+        },
       },
     },
   ],
   transformers: {
     remark: {
       // Global remark options
-    }
+    },
   },
 };
