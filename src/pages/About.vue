28f4da3e94773c1e589ee62fd51cfd982ea48719
phixyn <phixyn@gmail.com>
2020-10-11 15:48:11 +0100

    About: Replace Bulma message with Bulma card
    The message didn't make as much sense and we would have to override a bunch
    of variables to make it look decent. I think Card suits this purpose more and
    requires no overriding, just looks good out of the box.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,21 +4,27 @@
       <div class="container">
         <ContentBanner v-bind:banner="$page.about.banner"/>
 
-        <h2 class="block has-text-centered subtitle is-5" v-html="$page.about.hero_text"></h2>
+        <h2 class="block has-text-centered is-size-5" v-html="$page.about.hero_text"></h2>
 
-        <article class="message">
-          <div class="message-body">
-            <figure class="image is-128x128 is-pulled-right ml-4">
-              <img v-bind:src="$page.about.about_profile_pic">
-            </figure> <!-- .image -->
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <h2 class="title is-4"> {{ $page.about.bio_header }} </h2>
+                <div class="content" v-html="$page.about.bio"></div>
+                <div class="content has-text-centered" v-html="$page.about.social_links"></div>
+              </div> <!-- .media-content -->
 
-            <h2 class="title is-4"> {{ $page.about.bio_header }} </h2>
-            <div class="content" v-html="$page.about.bio"></div>
-            <div class="content has-text-centered" v-html="$page.about.social_links"></div>
-          </div> <!-- .message-body -->
-        </article> <!-- .message -->
+              <div class="media-right">
+                <figure class="image is-128x128 ml-4">
+                  <img v-bind:src="$page.about.about_profile_pic">
+                </figure> <!-- .image -->
+              </div> <!-- .media-right -->
+            </div> <!-- .media -->
+          </div> <!-- .card-content -->
+        </div> <!-- .card -->
 
-        <div class="content" v-html="$page.about.content"></div>
+        <div class="content mt-6" v-html="$page.about.content"></div>
       </div> <!-- .container -->
     </section>
   </Layout>
