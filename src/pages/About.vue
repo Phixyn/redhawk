14b8b1490d1400bbc0d6ef5b1594262747f2ad14
phixyn <phixyn@gmail.com>
2020-10-11 15:51:05 +0100

    Style: Format About.vue file

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,22 +2,28 @@
   <Layout>
     <section class="section">
       <div class="container">
-        <ContentBanner v-bind:banner="$page.about.banner"/>
+        <ContentBanner v-bind:banner="$page.about.banner" />
 
-        <h2 class="block has-text-centered is-size-5" v-html="$page.about.hero_text"></h2>
+        <h2
+          class="block has-text-centered is-size-5"
+          v-html="$page.about.hero_text"
+        ></h2>
 
         <div class="card">
           <div class="card-content">
             <div class="media">
               <div class="media-content">
-                <h2 class="title is-4"> {{ $page.about.bio_header }} </h2>
+                <h2 class="title is-4">{{ $page.about.bio_header }}</h2>
                 <div class="content" v-html="$page.about.bio"></div>
-                <div class="content has-text-centered" v-html="$page.about.social_links"></div>
+                <div
+                  class="content has-text-centered"
+                  v-html="$page.about.social_links"
+                ></div>
               </div> <!-- .media-content -->
 
               <div class="media-right">
                 <figure class="image is-128x128 ml-4">
-                  <img v-bind:src="$page.about.about_profile_pic">
+                  <img v-bind:src="$page.about.about_profile_pic" />
                 </figure> <!-- .image -->
               </div> <!-- .media-right -->
             </div> <!-- .media -->
