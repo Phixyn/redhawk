44e5f9629517d62d64d226df3e59d57641eb5ffa
phixyn <phixyn@gmail.com>
2021-02-21 23:16:58 +0000

    Components: Add a Hero component
    Add a component for the Hero section of the homepage. For now, this has some
    hardcoded text and an image. In the future, these should be passed via props.

---
+++ src/components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- TODO: Make background very dark grey, maybe with scoped style? -->
+  <section class="hero is-medium is-dark">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <!-- TODO center image -->
+        <figure class="image is-128x128">
+            <img
+              class="is-rounded"
+              src="https://cdn.phixyn.com/portfolio/me3.png"
+              alt="Profile picture"
+            />
+          </figure> <!-- .image -->
+
+        <!-- TODO pass via props -->
+        <p class="title">Alpeche Pancha</p>
+        <p class="subtitle mt-2">Full Stack Developer</p>
+        <!-- Prop "bodyText" -->
+        <p>
+          My name is Alpeche Pancha. I am a Full Stack Developer based in Suffolk, England. I have over 10 years of experience, including 5 years of professional experience, in software and web development. I have previously worked at a software consultancy company based at Adastral Park, Suffolk. I am currently looking for new opportunities in web development.
+        </p>
+        <!-- Prop "bodyFooter" ? -->
+        <!-- TODO make sure links are styled -->
+        <p><a href="#">Portfolio</a> // <a href="#">Download CV</a> // <a href="#">Contact me</a></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
