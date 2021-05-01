b35c0e7fcc2c6ba2a4154ec587de940210d60d2c
phixyn <phixyn@gmail.com>
2021-05-01 20:27:15 +0100

    Hero: Begin adding props to the component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,24 +3,32 @@
   <section class="hero is-medium is-dark">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <figure class="image is-inline-block">
-            <img
-              class="is-rounded"
-              src="https://cdn.phixyn.com/portfolio/me3.png"
-              alt="Profile picture"
-            />
-          </figure> <!-- .image -->
-
         <!-- TODO pass via props -->
-        <p class="title">Alpeche Pancha</p>
-        <p class="subtitle mt-2">Full Stack Developer</p>
+        <figure class="image is-inline-block">
+          <img
+            v-bind:class="{ 'is-rounded': imageIsRound }"
+            v-bind:src="imageSrc"
+            v-bind:alt="imageAlt"
+          />
+        </figure> <!-- .image -->
+
+        <p class="title">{{ title }}</p>
+        <p class="subtitle mt-2">{{ subtitle }}</p>
         <!-- Prop "bodyText" -->
         <p>
-          My name is Alpeche Pancha. I am a Full Stack Developer based in Suffolk, England. I have over 10 years of experience, including 5 years of professional experience, in software and web development. I have previously worked at a software consultancy company based at Adastral Park, Suffolk. I am currently looking for new opportunities in web development.
+          My name is Alpeche Pancha. I am a Full Stack Developer based in
+          Suffolk, England. I have over 10 years of experience, including 5
+          years of professional experience, in software and web development. I
+          have previously worked at a software consultancy company based at
+          Adastral Park, Suffolk. I am currently looking for new opportunities
+          in web development.
         </p>
         <!-- Prop "bodyFooter" ? -->
         <!-- TODO make sure links are styled -->
-        <p><a href="#">Portfolio</a> // <a href="#">Download CV</a> // <a href="#">Contact me</a></p>
+        <p>
+          <a href="#">Portfolio</a> // <a href="#">Download CV</a> //
+          <a href="#">Contact me</a>
+        </p>
       </div>
     </div>
   </section>
@@ -29,5 +37,30 @@
 <script>
 export default {
   name: "Hero",
+  props: {
+    imageSrc: {
+      type: String,
+      required: true,
+    },
+    imageAlt: {
+      type: String,
+      default: "Hero image",
+    },
+    imageIsRound: {
+      type: Boolean,
+      default: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    bodyText: {
+      type: String,
+    },
+  },
 };
 </script>
